a79f7b29329bca0a178261d640ab93749e45a1f3
longchenming <1264221821@qq.com>
2020-06-26 14:05:47 +0800

    init

---
+++ eureka/eureka-server/src/test/java/com/lcm/test/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lcm.test.eurekaserver;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
